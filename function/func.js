915b34a24a273a5d8b2bc6d3df3be2777d1468be
Dinesh51297 <dineshyadav681@gmail.com>
2018-04-03 17:45:52 +0530

    gv

--- function/func.js
+++ function/func.js
@@ -8,4 +8,10 @@ hi();
 hi();
 console.log("new commit 3");
 console.log("vyju");
+console.log("dinesh");
+console.log("dineshyada");
+
+console.log("new commit 3");
 console.log("vyju");
+console.log("dinesh");
+console.log("dineshyada");
