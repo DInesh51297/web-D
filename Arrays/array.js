9176aa1a536bd56db0e9350dea084b83f57b8ab9
Dinesh51297 <dineshyadav681@gmail.com>
2018-04-04 22:51:58 +0530

    methods

--- Arrays/array.js
+++ Arrays/array.js
@@ -7,7 +7,18 @@ var friends = new Array();
 
 var hi = [47,"dinesh",5,true,null];
 console.log(hi[3]);
+//want to add something 
+hi[5]=false;
+
+
 // length property
 
+hi.length
+
+//METHODS IN ARRAY 
+
+//PUSH and POP
+
+hi.push(65);
 console.log(hi.length);
 
