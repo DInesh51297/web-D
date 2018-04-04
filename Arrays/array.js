be9ef3227d9f5a3094c6ed5e60116cc5c397cf72
Dinesh51297 <dineshyadav681@gmail.com>
2018-04-04 22:42:51 +0530

    starting with arrays

---
+++ Arrays/array.js
@@ -0,0 +1,13 @@
+//declearing and initializing array
+var friends = [];
+var friends = new Array();
+
+//special thing in js 
+//array can hold any type of data 
+
+var hi = [47,"dinesh",5,true];
+
+// length property
+
+console.log(hi.length);
+
