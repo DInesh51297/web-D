2dce2204000b06dffd31e1be8514b486b4557d20
Dinesh51297 <dineshyadav681@gmail.com>
2018-04-04 12:59:48 +0530

    msb

--- function/func.js
+++ function/func.js
@@ -15,3 +15,12 @@ console.log("new commit 3");
 console.log("vyju");
 console.log("dinesh");
 console.log("dineshyada");
+var a = 15;
+ console.log(a+"dinesh yadav");
+ var ab = "Hello world";
+ console.log("ghar ja");
+for (var i=0;j<10;i++){
+    console.log(i);
+}
+console.log("sunny parihar");
+
